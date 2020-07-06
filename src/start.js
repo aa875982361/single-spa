7ed67071546c2caed657a7a6ede0c0f457b8111e
huangpeilin <875982361@qq.com>
2020-07-06 23:28:13 +0800

    feat: 添加说明

--- src/start.js
+++ src/start.js
@@ -4,12 +4,18 @@ import { setUrlRerouteOnly } from "./navigation/navigation-events.js";
 import { isInBrowser } from "./utils/runtime-environment.js";
 
 let started = false;
-
+/**
+ * 主要的启动函数
+ * @param {*} opts 
+ */
 export function start(opts) {
+  // 通过一个全局变量存储是否开始
   started = true;
+  // 通过参数是否有url路由只读，设置路由只读
   if (opts && opts.urlRerouteOnly) {
     setUrlRerouteOnly(opts.urlRerouteOnly);
   }
+  // 判读是否在浏览器中 如果是的话 就检查哪些子应用需要渲染 
   if (isInBrowser) {
     reroute();
   }
